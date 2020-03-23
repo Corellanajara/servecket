0004f65cf10e68fb5eb0aebb7326be458fd9d4d7
Cristopher Orellana <corellanajara@hotmail.com>
2020-03-23 19:04:51 -0300

    cambios locos pa probar slack

--- index.js
+++ index.js
@@ -1,22 +1,135 @@
-let express = require('express')
-let app = express();
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+var os = require('os');
+const { exec } = require("child_process");
+const readline = require("readline");
+const os_utils 	= require('os-utils');
+var request = require("request");
+var fs = require('fs');
+var login = [];
+var idUsuario = "No es un usuario real ni autenticado";
+fs.readFile('conf.txt','utf8', function(err, contents) {
+    var partes = contents.split('\n');
+    var usuario = ["user","pass","port"];
+    var login = [];
+    for(var i = 0 ; i < partes.length;i++){
+	     var parte = partes[i];
+	      var contenido = parte.split("=")[1];
+	      login.push(contenido);
+        if(i == partes.length -1){
+          iniciar(login);
+        }
+    }
+    console.log("pido iniciar");
+    console.log(login);
+});
 
-let http = require('http');
-let server = http.Server(app);
 
-let socketIO = require('socket.io');
-let io = socketIO(server);
-
-const port = process.env.PORT || 3000;
-
-io.on('connection', (socket) => {
-    console.log('user connected');
-
-    socket.on('new-message', (message) => {
-      console.log(message);
+function iniciar(l){
+  console.log("login",l);
+  var obj = {
+      "correo": "asfa"+l[0],
+      "clave": "asfa"+l[1]
+    }
+  console.log("obj",obj);
+  request.post({
+        "headers": { "content-type": "application/json" },
+        "url": "http://167.71.160.73:8080/api/usuarios/login/",
+        "body": JSON.stringify({
+            "correo": l[0],
+            "clave": l[1]
+        })
+    }, (error, response, body) => {
+        var resultado = JSON.parse(body);
+        if(error) {
+            return console.dir(error);
+        }
+        if(resultado.length == 1){
+          console.log("body",resultado);
+          idUsuario = resultado[0]._id;
+          console.log("id del usuario traido",idUsuario);
+          console.log("id del usuario traido",resultado._id);
+          console.log("id del usuario traido",resultado[0]);
+          server.listen(l[2]);
+        }else {
+          console.log("body",resultado.length);
+          throw "no pudo inicializarse correctamente";
+        }
     });
+}
+
+function serviceStatus(client,service){
+    exec("service "+service+" status", function(error, stdout){
+      if (error) {
+	        client.emit('res'+service.toUpperCase(),"0");
+          return;
+      }
+      var statuspmtatmp = /Active: [a-z]* [\(][a-z]*[\)]/g.exec(stdout.toString())[0];
+      /*
+      PARA CENTOS
+      */
+      if(service.toUpperCase() == "httpd"){
+        service = "apache2";
+      }
+      if(service.toUpperCase() == "mysqld"){
+        service = "mysql";
+      }
+      /*
+      */
+      client.emit('res'+service.toUpperCase(),"1")
+   });
+}
+
+io.on('connection', client => {
+  if(client.handshake.query.idUsuario == idUsuario){
+    serviceStatus(client,"mysql");
+    serviceStatus(client,"mysqld");
+    serviceStatus(client,"apache2");
+    serviceStatus(client,"httpd");
+    serviceStatus(client,"cron");
+    serviceStatus(client,"sshd");
+  }
+
+  client.on('estadoGrafico',data=>{
+//    console.log(idUsuario == data);
+    if(data == idUsuario){
+      client.emit('cpu',{data : os.cpus()});
+      client.emit('totalmem',{data :os.totalmem()});
+      client.emit('freemem',{data : os.freemem()});
+      os_utils.cpuUsage(function(v){
+        client.emit('cpuUsage',{data:v})
+      });
+      os_utils.cpuFree(function(v){
+        client.emit('cpuFree',{data:v})
+      });
+    }
+  })
+  client.on('estadoServicios',data=>{
+
+    if(data == idUsuario){
+      serviceStatus(client,"mysql");
+      serviceStatus(client,"mysqld");
+      serviceStatus(client,"apache2");
+      serviceStatus(client,"httpd");
+      serviceStatus(client,"cron");
+      serviceStatus(client,"sshd");
+    }
+
+  })
+  client.on('ejecutar', data=>{
+    exec(data, (error, stdout, stderr) => {
+        if (error) {
+            return;
+        }
+        if (stderr) {
+            return;
+        }
+        client.emit('res',{res:stdout});
+    });
+  })
+  client.on('notif',dato=>{
+    client.emit('notificacionLocal',{red:dato});
+  })
 });
 
-server.listen(port, () => {
-    console.log(`started on port: ${port}`);
-});
+//server.listen(4555);
--- socket.js
+++ socket.js
@@ -2,48 +2,130 @@ const server = require('http').createServer();
 const io = require('socket.io')(server);
 var os = require('os');
 const { exec } = require("child_process");
+const readline = require("readline");
+const os_utils 	= require('os-utils');
+var request = require("request");
 
-var segundos = 3000;
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+var idUsuario = "No es un usuario real ni autenticado";
+rl.question("Dame tu correo ", function(correo) {
+    rl.question("Clave ", function(clave) {
+          request.post({
+                "headers": { "content-type": "application/json" },
+                "url": "http://167.71.160.73:8080/api/usuarios/login/",
+                "body": JSON.stringify({
+                    "correo": correo,
+                    "clave": clave
+                })
+            }, (error, response, body) => {
+                var resultado = JSON.parse(body);
+                if(error) {
+                    return console.dir(error);
+                }
+                if(resultado.length == 1){
+                  console.log("body",resultado);
+                  idUsuario = resultado[0]._id;
+                  console.log("id del usuario traido",idUsuario);
+                  console.log("id del usuario traido",resultado._id);
+                  console.log("id del usuario traido",resultado[0]);
+                  rl.question("Super! que puerto uso? R : ", function(puerto) {
+                    var texto = "user="+correo;
+                    texto += "\npass="+clave;
+                    texto += "\nport="+puerto;
+                    fs = require('fs');
+                    fs.writeFile('conf.txt', texto, function (err) {
+                      if (err) return console.log(err);
+                      console.log('OK! ahora puedes ejecutar node index.js');
+                    });
+                    server.listen(puerto);
+                  })
+
+                }else {
+                  console.log("body",resultado.length);
+                  throw "no pudo inicializarse correctamente";
+                }
+            });
+    });
+});
+
+function serviceStatus(client,service){
+    exec("service "+service+" status", function(error, stdout){
+      if (error) {
+	        client.emit('res'+service.toUpperCase(),"0");
+          return;
+      }
+      var statuspmtatmp = /Active: [a-z]* [\(][a-z]*[\)]/g.exec(stdout.toString())[0];
+      /*
+      PARA CENTOS
+      */
+      if(service.toUpperCase() == "httpd"){
+        service = "apache2";
+      }
+      if(service.toUpperCase() == "mysqld"){
+        service = "mysql";
+      }
+      /*
+      */
+      client.emit('res'+service.toUpperCase(),"1")
+   });
+}
+
 io.on('connection', client => {
-  client.on('event', data => {
-        console.log(data);
-        console.log("es un evento dentro de la conexion");
-  });
-  var intervalo = setInterval(function(){
-    client.emit('cpu',{data : os.cpus()});
-    client.emit('totalmem',{data :os.totalmem()});
-    client.emit('freemem',{data : os.freemem()});
-  }, segundos );
-  client.on('setTiempo',data=>{
-    console.log(data);
-    segundos = data.segundos;
-    clearInterval(intervalo);
-    intervalo = setInterval(function(){
+  if(client.handshake.query.idUsuario == idUsuario){
+    serviceStatus(client,"mysql");
+    serviceStatus(client,"mysqld");
+    serviceStatus(client,"apache2");
+    serviceStatus(client,"httpd");
+    serviceStatus(client,"cron");
+    serviceStatus(client,"sshd");
+  }
+
+  client.on('estadoGrafico',data=>{
+//    console.log(idUsuario == data);
+    if(data == idUsuario){
       client.emit('cpu',{data : os.cpus()});
       client.emit('totalmem',{data :os.totalmem()});
       client.emit('freemem',{data : os.freemem()});
-    }, segundos );
+      os_utils.cpuUsage(function(v){
+        client.emit('cpuUsage',{data:v})
+      });
+      os_utils.cpuFree(function(v){
+        client.emit('cpuFree',{data:v})
+      });
+    }
+  })
+  client.on('estadoServicios',data=>{
+
+    if(data == idUsuario){
+      serviceStatus(client,"mysql");
+      serviceStatus(client,"mysqld");
+      serviceStatus(client,"apache2");
+      serviceStatus(client,"httpd");
+      serviceStatus(client,"cron");
+      serviceStatus(client,"sshd");
+    }
+
   })
   client.on('ejecutar', data=>{
-   console.log(data);
-//   data = data || "ls";/
-//	console.log(data);
     exec(data, (error, stdout, stderr) => {
         if (error) {
-            console.log(`error: ${error.message}`);
             return;
         }
         if (stderr) {
-            console.log(`stderr: ${stderr}`);
             return;
         }
-        console.log(`stdout: ${stdout}`);
         client.emit('res',{res:stdout});
     });
   })
-  client.on('disconnect', () => {
-      console.log("se fue el cliente");
-  });
+  client.on('notif',dato=>{
+    client.emit('notificacionLocal',{red:dato});
+  })
 });
-server.listen(5000);
 
+//server.listen(4555);
